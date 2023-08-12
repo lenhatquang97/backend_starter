f0ea1434cab116a222e7102389eaa6949b4ee66a
lenhatquang97 <lenhatquang97@gmail.com>
2023-08-12 14:31:29 +0700

    Change database source code

--- db.config.js
+++ db.config.js
@@ -1,16 +1,15 @@
 const mysql = require("mysql2");
-// exports.connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     port: 3306,
-//     database: "db_team_04",
-//     password: "Aa123456"
-// });
-//
 exports.connection = mysql.createConnection({
-    host: "118.102.2.130",
-    user: "db_team_04",
-    database: "db_team_04",
-    password: "F4u!o3LNI6DC",
-    port: 33306
+    host: "localhost",
+    user: "root",
+    port: 3306,
+    database: "db_test_04"
 });
+
+// exports.connection = mysql.createConnection({
+//     host: "118.102.2.130",
+//     user: "db_team_04",
+//     database: "db_team_04",
+//     password: "F4u!o3LNI6DC",
+//     port: 33306
+// });
